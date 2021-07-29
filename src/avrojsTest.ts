548ee7a3f1023ff75f7305abf1870455c3038289
Hana Worku <hana@truss.works>
2021-07-29 16:51:30 -0500

    Avro test

---
+++ src/avrojsTest.ts
@@ -0,0 +1,115 @@
+var avrojs = require("avro-js");
+var data = require("./domain-models/contractAmendmentInfo.ts");
+
+const avrojsType = avrojs.parse({
+  name: "ContractAmendmentInfo",
+  type: "record",
+  fields: [
+    {
+      name: "itemsBeingAmended",
+      type: {
+        type: "array",
+        items: {
+          name: "Item",
+          type: "enum",
+          symbols: [
+            "BENEFITS_PROVIDED",
+            "CAPITATION_RATES",
+            "ENCOUNTER_DATA",
+            "ENROLLE_ACCESS",
+            "ENROLLMENT_PROCESS",
+            "FINANCIAL_INCENTIVES",
+            "GEO_AREA_SERVED",
+            "GRIEVANCES_AND_APPEALS_SYSTEM",
+            "LENGTH_OF_CONTRACT_PERIOD",
+            "NON_RISK_PAYMENT",
+            "PROGRAM_INTEGRITY",
+            "QUALITY_STANDARDS",
+            "RISK_SHARING_MECHANISM",
+          ],
+        },
+      },
+    },
+    { name: "otherItemBeingAmended", type: ["null", "string"], default: null },
+    {
+      name: "capitationRatesAmendedInfo",
+      type: {
+        name: "CapitationRatesAmended",
+        type: "record",
+        fields: [
+          {
+            name: "AmendedReason",
+            type: {
+              name: "ReasonEnum",
+              type: "enum",
+              symbols: ["ANNUAL", "MIDYEAR", "OTHER"],
+            },
+          },
+          { name: "OtherReason", type: ["null", "string"], default: null },
+        ],
+      },
+    },
+  ],
+});
+
+console.log("--- testing avro-js ---");
+// serialize/deserialize
+// var buf = avrojsType.toBuffer(data.sampleContractAmendmentInfo1); // Serialized object.
+// var obj = avrojsType.fromBuffer(buf); // {kind: 'CAT', name: 'Albert'}
+// console.log(buf, obj);
+
+//Generate random instances of a schema. We might be able to use this for testing?!
+var type = avrojs.parse(avrojsType);
+var randomInstance = type.random(); // E.g. Buffer([48, 152, 2, 123])
+console.log("randomInstance: ", randomInstance);
+
+// Check validity
+console.log(
+  "valid type",
+  avrojsType.isValid(data.sampleContractAmendmentInfo1)
+);
+console.log(
+  "another valid type",
+  avrojsType.isValid(data.sampleContractAmendmentInfo2)
+);
+console.log(
+  "invalid type",
+  avrojsType.isValid(data.sampleContractAmendmentInfoInvalid)
+);
+
+// Calculate specific invalid fields for error handling
+function getInvalidPaths(
+  type: {
+    isValid: (arg0: any, arg1: { errorHook: (path: any) => void }) => void;
+  },
+  val: any
+) {
+  var paths: any[] = [];
+  type.isValid(val, {
+    errorHook: function (path) {
+      paths.push(path.join());
+    },
+  });
+  return paths;
+}
+
+var paths1 = getInvalidPaths(avrojsType, data.sampleContractAmendmentInfo1);
+var paths2 = getInvalidPaths(avrojsType, data.sampleContractAmendmentInfo2);
+
+var pathsInvalid = getInvalidPaths(
+  avrojsType,
+  data.sampleContractAmendmentInfoInvalid
+);
+
+console.log(
+  "this is an invalid instance",
+  data.sampleContractAmendmentInfoInvalid
+);
+console.log("invalidPaths: ", paths1);
+console.log("invalidPaths: ", paths2);
+console.log("invalidPaths: ", pathsInvalid);
+
+/* Notes:
+- for isValid checks to work properly the schema names need to make the object being passed in (including casing). This risks being another place we are doubling up on logic, another place to add a new field
+- could Avro also handle any conditional logic (e.g. no otherItemBeingAmended unless the itemsBeingAmended has the OTHER enum). Seems like no
+*/
---
+++ src/domain-models/contractAmendmentInfo.ts
@@ -0,0 +1,41 @@
+type ContractAmendmentInfo = {
+  itemsBeingAmended: string[];
+  otherItemBeingAmended?: string;
+  capitationRatesAmendedInfo?: {
+    reason?: "ANNUAL" | "MIDYEAR" | "OTHER";
+    otherReason?: string;
+  };
+  relatedToCovid19?: boolean;
+  relatedToVaccination?: boolean;
+};
+
+const sampleContractAmendmentInfo1 = {
+  itemsBeingAmended: [
+    "BENEFITS_PROVIDED",
+    "CAPITATION_RATES",
+    "ENROLLE_ACCESS",
+    "OTHER",
+  ],
+  otherItemBeingAmended: "This is why items amended",
+  capitationRatesAmendedInfo: {
+    reason: "OTHER",
+    otherReason: "This is capitation rates info",
+  },
+  relatedToCovid19: true,
+  relatedToVaccination: false,
+};
+
+const sampleContractAmendmentInfo2 = {
+  itemsBeingAmended: ["BENEFITS_PROVIDED"],
+  relatedToCovid19: false,
+};
+
+const sampleContractAmendmentInfoInvalid = {
+  itemsBeingAmended: ["INVALID_ENUM", "OTHER"],
+  otherItemBeingAmended: "This is why items amended",
+  relatedToCovid19: "a boolean",
+};
+
+exports.sampleContractAmendmentInfo1 = sampleContractAmendmentInfo1;
+exports.sampleContractAmendmentInfo2 = sampleContractAmendmentInfo2;
+exports.sampleContractAmendmentInfoInvalid = sampleContractAmendmentInfoInvalid;
